82d67dcaefa634c581ca3d60591318ba974a0016
kamarmack <kamarmack112@gmail.com>
2022-04-29 01:51:43 -0500

    inject tailwind styles via the prose component

--- components/Prose.tsx
+++ components/Prose.tsx
@@ -1,6 +1,14 @@
+import { cx } from '@/lib/utils';
+
 export const Prose: React.FC = ({ children }) => {
 	return (
-		<div className="max-w-none prose prose-a:text-pink-600 dark:prose-invert">
+		<div
+			className={cx(
+				'max-w-none prose hover:prose-a:opacity-90 prose-code:rounded prose-code:px-1 prose-code:font-medium',
+				'prose-a:text-blue-900 prose-code:bg-gray-200',
+				'dark:prose-invert dark:prose-a:text-blue-600 dark:prose-code:bg-gray-800',
+			)}
+		>
 			{children}
 		</div>
 	);
